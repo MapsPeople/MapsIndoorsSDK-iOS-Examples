d8a87e023842802396666244fb30a14cbc84ce48
Christian Wolf Johannsen <chwo@mapspeople.com>
2024-11-05 14:00:39 +0100

    Apply swiftformat

--- MapsIndoorsSDK-iOS-Examples/Intermediate/Show My Location/MyPositionProvider.swift
+++ MapsIndoorsSDK-iOS-Examples/Intermediate/Show My Location/MyPositionProvider.swift
@@ -1,55 +1,54 @@
-import UIKit
-import MapsIndoors
 import CoreLocation
+import MapsIndoors
 
-class MyPositionProvider : NSObject, MPPositionProvider {
-    
+class MyPositionProvider: MPPositionProvider {
     var delegate: MPPositionProviderDelegate?
     var latestPosition: MPPositionResult?
     private var running = false
-    
+
     init(mockAt: MPPoint) {
-        latestPosition = MPPositionResult(coordinate: CLLocationCoordinate2D(latitude: mockAt.latitude, longitude: mockAt.longitude) , floorIndex: 10, accuracy: 20.0)
+        latestPosition = MPPositionResult(coordinate: CLLocationCoordinate2D(latitude: mockAt.latitude, longitude: mockAt.longitude), floorIndex: 10, accuracy: 20.0)
     }
+
     func updatePosition() {
-        if running {
-            latestPosition?.bearing = (latestPosition!.bearing + 10)
-            latestPosition?.coordinate = generateRandomCoordinate()
-            latestPosition?.bearing = Double.random(in: 0..<360)
-            latestPosition?.accuracy = Double.random(in: 0..<10)
-            
-            if let delegate = self.delegate, let latestPosition = self.latestPosition {
-                delegate.onPositionUpdate(position: latestPosition)
-            }
-            
-            weak var _self = self
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                _self?.updatePosition()
-            }
+        guard running else { return }
+
+        latestPosition?.bearing = (latestPosition!.bearing + 10)
+        latestPosition?.coordinate = generateRandomCoordinate()
+        latestPosition?.bearing = Double.random(in: 0 ..< 360)
+        latestPosition?.accuracy = Double.random(in: 0 ..< 10)
+
+        if let delegate, let latestPosition {
+            delegate.onPositionUpdate(position: latestPosition)
+        }
+
+        Task { @MainActor [weak self] in
+            try await Task.sleep(nanoseconds: 1_000_000_000)
+            guard let self else { return }
+
+            updatePosition()
         }
     }
 
-    func startPositioning(_ arg: String?) {
+    func startPositioning(_: String?) {
         running = true
         updatePosition()
     }
 
-    func stopPositioning(_ arg: String?) {
+    func stopPositioning(_: String?) {
         running = false
     }
-    
+
     func generateRandomCoordinate() -> CLLocationCoordinate2D {
         // Define a small range for the random offset
-        let offset: Double = 0.00001
-        
+        let offset = 0.00001
+
         // Generate random offsets for latitude and longitude
-        let latOffset = Double.random(in: -offset...offset)
-        let lonOffset = Double.random(in: -offset...offset)
-        
+        let latOffset = Double.random(in: -offset ... offset)
+        let lonOffset = Double.random(in: -offset ... offset)
+
         // Create a new coordinate with the random offsets
         let newCoordinate = CLLocationCoordinate2D(latitude: (latestPosition?.coordinate.latitude ?? 0) + latOffset, longitude: (latestPosition?.coordinate.longitude ?? 0) + lonOffset)
         return newCoordinate
     }
-    
 }
--- MapsIndoorsSDK-iOS-Examples/Intermediate/Show My Location/ShowMyLocationController.swift
+++ MapsIndoorsSDK-iOS-Examples/Intermediate/Show My Location/ShowMyLocationController.swift
@@ -1,46 +1,43 @@
-import UIKit
-import MapsIndoorsCore
 import MapsIndoors
+import MapsIndoorsCore
+import UIKit
 
 /***
  ---
  title: Show the Blue Dot with MapsIndoors - Part 2
  ---
- 
+
  This is part 2 of the tutorial of managing a blue dot on the map. [In Part 1 we created the position provider](showmylocationmypositionprovider). Now we will create a view controller displaying a map that shows the users (mock) location.
- 
+
  Create a class `ShowMyLocationController` that inherits from `BaseMapController`.
  ***/
 class ShowMyLocationController: BaseMapController {
-
     var originalIcon: UIImage?
-    
+
     var locationButton: UIButton!
-    
+
     override func setupController() async {
-        
         if let validPosition = await getRandomLocation()?.position {
             let provider = MyPositionProvider(mockAt: validPosition)
-            
+
             MPMapsIndoors.shared.positionProvider = provider
             mapControl?.showUserPosition = true
-            
+
             provider.startPositioning(nil)
-            
-            if let dr = MPMapsIndoors.shared.displayRuleFor(displayRuleType: .blueDot)
-            {
+
+            if let dr = MPMapsIndoors.shared.displayRuleFor(displayRuleType: .blueDot) {
                 originalIcon = dr.icon
             }
         }
         setupLocationButton()
     }
-    
+
     func setupLocationButton() {
         locationButton = UIButton(type: .system)
         locationButton.setTitle("Toggle Icon", for: .normal)
-        locationButton.backgroundColor = UIColor(red: 75/255, green: 125/255, blue: 124/255, alpha: 0.6)
+        locationButton.backgroundColor = UIColor(red: 75 / 255, green: 125 / 255, blue: 124 / 255, alpha: 0.6)
         locationButton.addTarget(self, action: #selector(changeIcon), for: .touchUpInside)
-        
+
         // Create a stack view
         let stackView = UIStackView(arrangedSubviews: [locationButton])
         stackView.axis = .vertical
@@ -48,14 +45,14 @@ class ShowMyLocationController: BaseMapController {
         stackView.spacing = 10
         stackView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stackView)
-        
+
         // Add constraints
         NSLayoutConstraint.activate([
             stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
             stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -60),
         ])
     }
-    
+
     @objc func changeIcon() {
         if let dr = MPMapsIndoors.shared.displayRuleFor(displayRuleType: .blueDot) {
             if dr.icon == originalIcon {
@@ -65,40 +62,41 @@ class ShowMyLocationController: BaseMapController {
             }
         }
     }
+
     // create a blue colored dot
     func createLocationMarker() -> UIImage {
         let size = CGSize(width: 25, height: 25)
-        
+
         UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
         let context = UIGraphicsGetCurrentContext()
-        
+
         // Draw the blue dot
         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         context?.setFillColor(UIColor.blue.cgColor)
         context?.fillEllipse(in: rect)
-        
+
         // Draw the arrow
         context?.setFillColor(UIColor.white.cgColor) // Arrow color
         let arrowWidth: CGFloat = 6.0
         let arrowHeight: CGFloat = 10.0
         let arrowPath = UIBezierPath()
-        arrowPath.move(to: CGPoint(x: size.width/2, y: 5)) // Top point of the arrow
-        arrowPath.addLine(to: CGPoint(x: size.width/2 - arrowWidth/2, y: 5 + arrowHeight))
-        arrowPath.addLine(to: CGPoint(x: size.width/2 + arrowWidth/2, y: 5 + arrowHeight))
+        arrowPath.move(to: CGPoint(x: size.width / 2, y: 5)) // Top point of the arrow
+        arrowPath.addLine(to: CGPoint(x: size.width / 2 - arrowWidth / 2, y: 5 + arrowHeight))
+        arrowPath.addLine(to: CGPoint(x: size.width / 2 + arrowWidth / 2, y: 5 + arrowHeight))
         arrowPath.close()
         context?.addPath(arrowPath.cgPath)
         context?.fillPath()
-        
+
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
+
         return image ?? UIImage()
     }
-    
+
     override func adjustUI(forMenu: Bool) {
         locationButton.isHidden = forMenu
     }
-    
+
     override func updateForBuildingChange() {
         if locationButton != nil {
             locationButton.isHidden = false
